e24f96765a8c1b8aeaab47e437d93bf55edf4c3f
OleksandrBuievych <112573653+OleksandrBuievych@users.noreply.github.com>
2023-06-26 01:23:20 +0300

    Добавьте файлы проекта.

---
+++ Laba_31_OOP/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿
+namespace Laba_31_OOP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.processListView = new System.Windows.Forms.ListView();
+            this.infoBtn = new System.Windows.Forms.Button();
+            this.stopBtn = new System.Windows.Forms.Button();
+            this.exportBtn = new System.Windows.Forms.Button();
+            this.threadBtn = new System.Windows.Forms.Button();
+            this.modylesBtn = new System.Windows.Forms.Button();
+            this.addProcessButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // processListView
+            // 
+            this.processListView.HideSelection = false;
+            this.processListView.Location = new System.Drawing.Point(401, 28);
+            this.processListView.Name = "processListView";
+            this.processListView.Size = new System.Drawing.Size(805, 456);
+            this.processListView.TabIndex = 0;
+            this.processListView.UseCompatibleStateImageBehavior = false;
+            // 
+            // infoBtn
+            // 
+            this.infoBtn.Location = new System.Drawing.Point(123, 259);
+            this.infoBtn.Name = "infoBtn";
+            this.infoBtn.Size = new System.Drawing.Size(138, 66);
+            this.infoBtn.TabIndex = 2;
+            this.infoBtn.Text = "Інформація про процес";
+            this.infoBtn.UseVisualStyleBackColor = true;
+            this.infoBtn.Click += new System.EventHandler(this.infoBtn_Click);
+            // 
+            // stopBtn
+            // 
+            this.stopBtn.Location = new System.Drawing.Point(123, 356);
+            this.stopBtn.Name = "stopBtn";
+            this.stopBtn.Size = new System.Drawing.Size(138, 59);
+            this.stopBtn.TabIndex = 3;
+            this.stopBtn.Text = "Зупинити процес";
+            this.stopBtn.UseVisualStyleBackColor = true;
+            this.stopBtn.Click += new System.EventHandler(this.stopBtn_Click);
+            // 
+            // exportBtn
+            // 
+            this.exportBtn.Location = new System.Drawing.Point(123, 445);
+            this.exportBtn.Name = "exportBtn";
+            this.exportBtn.Size = new System.Drawing.Size(138, 63);
+            this.exportBtn.TabIndex = 4;
+            this.exportBtn.Text = "Експорт";
+            this.exportBtn.UseVisualStyleBackColor = true;
+            this.exportBtn.Click += new System.EventHandler(this.exportBtn_Click);
+            // 
+            // threadBtn
+            // 
+            this.threadBtn.Location = new System.Drawing.Point(123, 533);
+            this.threadBtn.Name = "threadBtn";
+            this.threadBtn.Size = new System.Drawing.Size(138, 65);
+            this.threadBtn.TabIndex = 5;
+            this.threadBtn.Text = "Потоки процесу";
+            this.threadBtn.UseVisualStyleBackColor = true;
+            this.threadBtn.Click += new System.EventHandler(this.threadBtn_Click);
+            // 
+            // modylesBtn
+            // 
+            this.modylesBtn.Location = new System.Drawing.Point(123, 630);
+            this.modylesBtn.Name = "modylesBtn";
+            this.modylesBtn.Size = new System.Drawing.Size(138, 65);
+            this.modylesBtn.TabIndex = 6;
+            this.modylesBtn.Text = "Модулі процесу";
+            this.modylesBtn.UseVisualStyleBackColor = true;
+            this.modylesBtn.Click += new System.EventHandler(this.modylesBtn_Click);
+            // 
+            // addProcessButton
+            // 
+            this.addProcessButton.Location = new System.Drawing.Point(123, 108);
+            this.addProcessButton.Name = "addProcessButton";
+            this.addProcessButton.Size = new System.Drawing.Size(138, 82);
+            this.addProcessButton.TabIndex = 7;
+            this.addProcessButton.Text = "Додати процес";
+            this.addProcessButton.UseVisualStyleBackColor = true;
+            this.addProcessButton.Click += new System.EventHandler(this.addProcessButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1363, 752);
+            this.Controls.Add(this.addProcessButton);
+            this.Controls.Add(this.modylesBtn);
+            this.Controls.Add(this.threadBtn);
+            this.Controls.Add(this.exportBtn);
+            this.Controls.Add(this.stopBtn);
+            this.Controls.Add(this.infoBtn);
+            this.Controls.Add(this.processListView);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView processListView;
+        private System.Windows.Forms.Button infoBtn;
+        private System.Windows.Forms.Button stopBtn;
+        private System.Windows.Forms.Button exportBtn;
+        private System.Windows.Forms.Button threadBtn;
+        private System.Windows.Forms.Button modylesBtn;
+        private System.Windows.Forms.Button addProcessButton;
+    }
+}
+
---
+++ Laba_31_OOP/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Laba_31_OOP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+    }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            RefreshProcessList();
+        }
+
+        private void RefreshProcessList()
+        {
+            processListView.Items.Clear();
+            Process[] processes = Process.GetProcesses();
+            foreach (Process process in processes)
+            {
+                ListViewItem item = new ListViewItem(process.Id.ToString());
+                item.SubItems.Add(process.ProcessName);
+                item.SubItems.Add(process.MainWindowTitle);
+                processListView.Items.Add(item);
+            }
+        }
+
+        private void infoBtn_Click(object sender, EventArgs e)
+        {
+            if (processListView.SelectedItems.Count > 0)
+            {
+                ListViewItem selectedItem = processListView.SelectedItems[0];
+                int processId = int.Parse(selectedItem.Text);
+                Process process = Process.GetProcessById(processId);
+                MessageBox.Show($"Інформація про процес:\n\nID: {process.Id}\nНазва: {process.ProcessName}\nЗаголовок: {process.MainWindowTitle}");
+            }
+        }
+
+        private void stopBtn_Click(object sender, EventArgs e)
+        {
+            if (processListView.SelectedItems.Count > 0)
+            {
+                ListViewItem selectedItem = processListView.SelectedItems[0];
+                int processId = int.Parse(selectedItem.Text);
+                Process process = Process.GetProcessById(processId);
+                process.Kill();
+                RefreshProcessList();
+            }
+        }
+
+        private void exportBtn_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Текстові файли (*.txt)|*.txt";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamWriter writer = new StreamWriter(saveFileDialog.FileName))
+                {
+                    foreach (ListViewItem item in processListView.Items)
+                    {
+                        writer.WriteLine($"ID: {item.SubItems[0].Text}, Назва: {item.SubItems[1].Text}, Заголовок: {item.SubItems[2].Text}");
+                    }
+                }
+            }
+        }
+
+        private void threadBtn_Click(object sender, EventArgs e)
+        {
+            if (processListView.SelectedItems.Count > 0)
+            {
+                ListViewItem selectedItem = processListView.SelectedItems[0];
+                int processId = int.Parse(selectedItem.Text);
+                Process process = Process.GetProcessById(processId);
+
+                string threadsInfo = "Потоки процесу:\n\n";
+                foreach (ProcessThread thread in process.Threads)
+                {
+                    threadsInfo += $"ID: {thread.Id}, Стан: {thread.ThreadState}\n";
+                }
+
+                MessageBox.Show(threadsInfo);
+            }
+        }
+
+        private void modylesBtn_Click(object sender, EventArgs e)
+        {
+            if (processListView.SelectedItems.Count > 0)
+            {
+                ListViewItem selectedItem = processListView.SelectedItems[0];
+                int processId = int.Parse(selectedItem.Text);
+                Process process = Process.GetProcessById(processId);
+
+                string modulesInfo = "Модулі процесу:\n\n";
+                foreach (ProcessModule module in process.Modules)
+                {
+                    modulesInfo += $"Назва: {module.ModuleName}, Версія: {module.FileVersionInfo.FileVersion}\n";
+                }
+
+                MessageBox.Show(modulesInfo);
+            }
+        }
+
+        private void addProcessButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                Process.Start(openFileDialog.FileName);
+                RefreshProcessList();
+            }
+        }
+    }
+}
